8d56a6a87e9e5362315807bdd144e85e5d7b9129
Worker <ipz243_yaiv@student.ztu.edu.ua>
2026-05-06 01:01:07 +0300

    iterator fix

--- KPZMKR_1/obj/Debug/net8.0/KPZMKR_1.AssemblyInfo.cs
+++ KPZMKR_1/obj/Debug/net8.0/KPZMKR_1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KPZMKR_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdfc9c3e73386d6e43d619c37e60daabfdc8d7a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf0ef60f09dc09c184c442ee48b6eb4cf6256473")]
 [assembly: System.Reflection.AssemblyProductAttribute("KPZMKR_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KPZMKR_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
